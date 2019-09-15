73c68663a02d3e1e20048737663a9da1c222d6ee
albertbchung <albertc435@gmail.com>
2019-09-15 15:51:49 -0400

    intial inheritance

---
+++ Assets/OtherMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherMovement : PlayerMovement
+{
+
+
+  protected override void LogTest(){
+    Debug.Log("Other Jump");
+  }
+}
