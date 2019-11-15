a7e38b7e908189cf2e07414a8c4d6f8dc64cb6b6
Anna Nguyen <axn3@cornell.edu>
2019-11-15 16:20:10 -0500

    added sound scripts.

---
+++ Scripts/Sound/CoinScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    public AudioSource ching;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ching = GetComponent<AudioSource> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            ching.Play();
+        }
+    }
+}
---
+++ Scripts/Sound/SwitchMusicTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMusicTrigger : MonoBehaviour
+{
+    public AudioClip newTrack;
+    private AudioManager AM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AM = FindObjectOfType<AudioManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if(newTrack != null) AM.changeBGM(newTrack);
+        }
+    }
+}
