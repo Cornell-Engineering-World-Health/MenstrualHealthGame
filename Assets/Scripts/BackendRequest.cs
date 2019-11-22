6327bd8e5f69ca62f5fbb78f9676a6976aa40af2
Teddy <klausnerteddy@gmail.com>
2019-11-22 17:45:43 -0500

    POST and GET

--- Assets/Scripts/BackendRequest.cs
+++ Assets/Scripts/BackendRequest.cs
@@ -9,25 +9,74 @@ public class BackendRequest : MonoBehaviour
     {
         StartCoroutine(GetRequest("http://localhost:3000/api/quiz/"));
 
+
+
+        //StartCoroutine(PostRequest("http://localhost:3000/api/quiz/"));
+    }
+
+    IEnumerator PostRequest(string uri, WWWForm data)
+    {
+        using (UnityWebRequest www = UnityWebRequest.Post(uri, data))
+        {
+            yield return www.SendWebRequest();
+
+            if(www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Post complete");
+            }
+
+        }
     }
 
     IEnumerator GetRequest(string uri)
-    {
+    { 
         using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
         {
             yield return webRequest.SendWebRequest();
             string[] pages = uri.Split('/');
             int page = pages.Length - 1;
-            Debug.Log(pages[page]);
-
             if (webRequest.isNetworkError)
             {
                 Debug.Log(pages[page] + ": Error: " + webRequest.error);
             }
             else
             {
-                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                string data = webRequest.downloadHandler.text;
+                Debug.Log(pages[page] + ":\nReceived: " + data);
             }
         }
     }
+
+    /*
+    Hashtable ParseRequest(string data)
+    {
+        string[] items = data.Split('}');
+        foreach(string item in items)
+        {
+            string key = item.Substring(item.IndexOf(':') + 2);
+            key = key.Substring(0, key.IndexOf('"'));
+
+            //float score = item
+        }
+    }
+
+    private class Data
+    {
+        string question, answer;
+        float score;
+
+        Data(string quest, string ans, float score)
+        {
+            question = quest;
+            answer = ans;
+            this.score = score;
+        }
+    }
+    */
+        
 }
+//[{"_id":"5dd80809205214432d80ed78","points":10,"answer":"25","question":"What is 5 * 5?","__v":0},{"_id":"5dd8081f205214432d80ed79","points":0,"answer":"55","question":"What is 11 * 5?","__v":0}]
