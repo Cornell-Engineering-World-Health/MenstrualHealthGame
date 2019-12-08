282611dd79285c38d599e22e6938a90d2e3408cc
albertbchung <albertc435@gmail.com>
2019-12-08 15:05:22 -0500

    Select 2 options

---
+++ Assets/Scripts/QuizOptionHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizOptionHandler : MonoBehaviour
+{
+    public GameObject questionPanel;
+    public GameObject correctPanel;
+    public GameObject incorrectPanel;
+    int correctCounter;
+
+    public void Correct() {
+        Debug.Log("correct");
+        correctCounter += 1;
+        if (correctCounter == 2) {
+            correctPanel.SetActive(true);
+            questionPanel.SetActive(false);
+        }
+    }
+
+    public void Incorrect()
+    {
+        Debug.Log("incorrect");
+        incorrectPanel.SetActive(true);
+        questionPanel.SetActive(false);
+    }
+}
